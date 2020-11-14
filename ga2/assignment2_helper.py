2998ba05ff646fee93e1b9bd952be36647099f66
Robert Detjens <detjensrobert@gmail.com>
2020-11-13 20:33:50 -0800

    remove print statements, add more test files
    Signed-off-by: Robert Detjens <detjensrobert@gmail.com>

--- ga2/assignment2_helper.py
+++ ga2/assignment2_helper.py
@@ -6,8 +6,6 @@ import assignment2
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
-print("------")
 max_score = assignment2.vankin_max_score(infile, outfile)
-print("------")
 
 print(f"Max score: {max_score}")
