0d27f887655f2d73c9f620f460905a2082c3ccd8
Robert Detjens <detjensrobert@gmail.com>
2020-11-13 20:56:31 -0800

    add test files
    Signed-off-by: Robert Detjens <detjensrobert@gmail.com>

--- ga3/assignment3_helper.py
+++ ga3/assignment3_helper.py
@@ -3,7 +3,13 @@ import sys
 
 import assignment3
 
+if len(sys.argv) == 1 or len(sys.argv) > 3:
+    print(f"Usage: {sys.argv[0]} infile (outfile, opt.) ")
+    exit(1)
+
 infile = sys.argv[1]
-outfile = sys.argv[2] or "/dev/null"
+outfile = sys.argv[2] if len(sys.argv) >= 3 else "/dev/null"
 
 min_moves = assignment3.vidrach_itky_leda(infile, outfile)
+
+print(f"Min moves: {min_moves}")
