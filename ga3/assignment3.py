6a74b22f02f9b819ee6176e8a6fc9f97d520a39d
Robert Detjens <detjensrobert@gmail.com>
2020-11-13 23:38:59 -0800

    some updates, closer but still broken
    Signed-off-by: Robert Detjens <detjensrobert@gmail.com>

--- ga3/assignment3.py
+++ ga3/assignment3.py
@@ -37,7 +37,7 @@ def vidrach_itky_leda(input_file_path, output_file_path):
     print(f"Board size: {board_size}x{board_size}")
     print(numpy.matrix(board))
 
-    moves = calculate_best_move(board, red_pos, blue_pos, history)
+    moves = gen_all_moves(board, red_pos, blue_pos, [])
 
     outfile.write(str(moves))
     infile.close()
@@ -45,88 +45,140 @@ def vidrach_itky_leda(input_file_path, output_file_path):
     return moves
 
 
-def calculate_best_move(board, red_pos, blue_pos, history):
+def gen_all_moves(board, red_pos, blue_pos, previous_moves, indent=0):
     """
-    Returns the minimum number of moves needed to solve VIL.
-    Works recursively and (hopefully) memoizes as to not be awful.
+    Walks through all possible moves from the current position.
+    Adds all movesets that end at the final swapped state to successful_moves.
     """
 
-    print(f"Checking r:{red_pos} b:{blue_pos}")
+    indent = indent + 4
 
-    move_results = []
+    board_size = len(board)
 
-    # ===================
-    # check red positions
-    # ===================
+    current_moves = previous_moves + [(red_pos, blue_pos)]
 
-    if red_pos != (
-        len(board) - 1,
-        len(board) - 1,
-    ):  # if red has made it, no need to move it anymore
+    print(" " * indent + f"Checking r{red_pos} b{blue_pos}")
+    a = [["-" for _ in row] for row in board]
+    a[red_pos[0]][red_pos[1]] = "R"
+    a[blue_pos[0]][blue_pos[1]] = "B"
+    # print(numpy.matrix(a))
+    print(" " * indent + f"Current move history: {current_moves}")
+
+    # if we are at the end state, return the current moveset that got us here
+    if red_pos == (board_size - 1, board_size - 1) and blue_pos == (0, 0):
+        print(" " * indent + "Reached the end!")
+        return current_moves
+
+    # otherwise, explore all moves we can do from here
+    # if we run into a state we have already been to,
+    # stop as its already been searched.
+
+    move_results = [current_moves]
+
+    # check all red moves
+    if red_pos != (board_size - 1, board_size - 1):
         move_dist = board[red_pos[0]][red_pos[1]]
 
         # up
         new_pos = (red_pos[0], red_pos[1] - move_dist)
-        if new_pos[1] >= 0 and new_pos != blue_pos:
-            move_results.append(calc_move(board, new_pos, blue_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[1] >= 0
+            and new_pos != blue_pos
+            and (new_pos, blue_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
         # down
         new_pos = (red_pos[0], red_pos[1] + move_dist)
-        if new_pos[1] < len(board) and new_pos != blue_pos:
-            move_results.append(calc_move(board, new_pos, blue_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[1] < len(board)
+            and new_pos != blue_pos
+            and (new_pos, blue_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
         # left
         new_pos = (red_pos[0] - move_dist, red_pos[1])
-        if new_pos[0] >= 0 and new_pos != blue_pos:
-            move_results.append(calc_move(board, new_pos, blue_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[0] >= 0
+            and new_pos != blue_pos
+            and (new_pos, blue_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
         # right
         new_pos = (red_pos[0] + move_dist, red_pos[1])
-        if new_pos[0] < len(board) and new_pos != blue_pos:
-            move_results.append(calc_move(board, new_pos, blue_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[0] < len(board)
+            and new_pos != blue_pos
+            and (new_pos, blue_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
-    # ===================
-    # check blue positions
-    # ===================
-
-    if blue_pos != (0, 0):  # if blue has made it, no need to move it anymore
-
+    # check all blue moves
+    if blue_pos != (0, 0):
         move_dist = board[blue_pos[0]][blue_pos[1]]
 
         # up
         new_pos = (blue_pos[0], blue_pos[1] - move_dist)
-        if new_pos[1] >= 0 and new_pos != blue_pos:
-            move_results.append(calc_move(board, red_pos, new_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[1] >= 0
+            and new_pos != red_pos
+            and (red_pos, new_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
         # down
         new_pos = (blue_pos[0], blue_pos[1] + move_dist)
-        if new_pos[1] < len(board) and new_pos != blue_pos:
-            move_results.append(calc_move(board, red_pos, new_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[1] < len(board)
+            and new_pos != red_pos
+            and (red_pos, new_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
         # left
         new_pos = (blue_pos[0] - move_dist, blue_pos[1])
-        if new_pos[0] >= 0 and new_pos != blue_pos:
-            move_results.append(calc_move(board, red_pos, new_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[0] >= 0
+            and new_pos != red_pos
+            and (red_pos, new_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
         # right
         new_pos = (blue_pos[0] + move_dist, blue_pos[1])
-        if new_pos[0] < len(board) and new_pos != blue_pos:
-            move_results.append(calc_move(board, red_pos, new_pos, history))
+        if (  # if in-bounds, not occupied, and has not been visited before
+            new_pos[0] < len(board)
+            and new_pos != red_pos
+            and (red_pos, new_pos) not in previous_moves
+        ):
+            r = gen_all_moves(board, new_pos, blue_pos, current_moves, indent)
+            if len(r) > 0:
+                move_results.append(r)
 
-    best_move_count = min(move_results)
-    return best_move_count
+    return move_results
 
-
-def calc_move(board, red_pos, blue_pos, history):
     """
-    Returns the the cached result for the passed state, or calls
-    calculate_best_move if the cache misses.
-    """
+    tree
+    of all moves
+    remove leafs that do not end
 
-    if (red_pos, blue_pos) in history:
-        print(f"Hit cache: f{(red_pos, blue_pos)}")
-        return history[(red_pos, blue_pos)]
-    else:
-        r = calculate_best_move(board, red_pos, blue_pos, history)
-        history[(red_pos, blue_pos)] = r
-        return r
+    return true if at end state
+    return false if we've been everywhere
+    recursively call
+    """
